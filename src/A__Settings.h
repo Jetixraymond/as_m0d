3e7779ec9f84a6250e51b66ac95e1ccb10e62da8
CarlCas <dda3004@gmail.com>
2017-03-11 00:06:10 +0700

    Add AMainThread, tracer, bugs...
    Prev

--- src/A__Settings.h
+++ src/A__Settings.h
@@ -1,12 +1,43 @@
 #pragma once
 
+struct Trace
+{
+	float start[3], end[3];
+	DWORD color, time;
+	Trace(float *st, float *en, DWORD col, DWORD tim)
+	{
+		memcpy(start, st, sizeof(float[3]));
+		memcpy(end, en, sizeof(float[3]));
+		color = col;
+		time = tim;
+	}
+};
+
+struct CheckerPosition
+{
+	long x, y;
+};
+
+struct stIpInfo
+{
+	std::string City, Provider;
+	float pos[2];
+};
+
+struct PlayerChecker
+{
+	std::string nick, reason;
+};
+
 struct AdminSetting
 {
+	std::vector<USHORT> PlayersIDForTP;
+	std::vector<Trace> Tracers;
+	std::vector<std::string> AdminChecker;
+	std::vector<PlayerChecker> PlayerChecker;
+
 	/*4 bytes*/
-	struct CheckerPosition
-	{
-		long x, y;
-	} ACheckPos, PCheckPos, KillListPos;
+	CheckerPosition ACheckPos, PCheckPos, KillListPos;
 	DWORD color_tracer_hit;
 	DWORD color_tracer;
 #pragma region ChatColor
@@ -16,12 +47,20 @@ struct AdminSetting
 	DWORD support;
 	DWORD feedback;
 #pragma endregion
+	int iHpCount = 100;
+	int iAmmoCount = 100;
 
 	/*2 bytes*/
 	unsigned short usSetLeaderID = -1;
+	unsigned short usMaxPlayerID = 0;
+	unsigned short usMaxPlayerTP = 0;
+	unsigned short usTraceID = 0;
+	unsigned short usTraceMaxCount = 1;
 
 	/*1 bytes*/
 	unsigned char byteFrackID = 0;
+	unsigned char byteWeaponID = 0;
+	unsigned char byteSkillWeaponID = 0;
 
 #pragma region ChatColorControl
 	bool chatcolor = true;
@@ -33,6 +72,11 @@ struct AdminSetting
 	bool chatcolors_support = true;
 #pragma endregion
 	bool traces = false;
+	bool bMassHP = false;
+	bool bGiveGuns = false;
+	bool bSkillGun = false;
+	bool bMassTP = false;
+	bool bTraceAll = true;
 };
 
 extern struct AdminSetting A_Set;
